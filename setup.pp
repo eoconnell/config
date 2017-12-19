6eb25692c2e222c2181798f185edebcdb3b0ba57
Evan O'Connell <oconn.e@gmail.com>
2017-12-18 21:36:00 -0500

    Remove package manager

--- setup.pp
+++ setup.pp
@@ -18,7 +18,6 @@ node 'burrito' {
      $packages.each |String $package| {
          package { "${package}":
              ensure   => installed,
-             provider => 'yum',
          }
      }
 
